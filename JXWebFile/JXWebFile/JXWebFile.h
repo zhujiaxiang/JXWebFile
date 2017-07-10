a7ef413c6aceb30c8a9d44e0f72230bd9129cd3c
zhujiaxiang <zjxbaozoudhm@gmail.com>
2017-07-10 14:28:15 +0800

    Init
    Init the project

---
+++ JXWebFile/JXWebFile/JXWebFile.h
@@ -0,0 +1,13 @@
+//
+//  JXWebFile.h
+//  JXWebFile
+//
+//  Created by 朱佳翔 on 2017/7/10.
+//  Copyright © 2017年 zjx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JXWebFile : NSObject
+
+@end
---
+++ JXWebFile/JXWebFile/JXWebFile.m
@@ -0,0 +1,13 @@
+//
+//  JXWebFile.m
+//  JXWebFile
+//
+//  Created by 朱佳翔 on 2017/7/10.
+//  Copyright © 2017年 zjx. All rights reserved.
+//
+
+#import "JXWebFile.h"
+
+@implementation JXWebFile
+
+@end
